c943397cb780cd7b3d12241f45de0dab632a1155
almayomekonen <alamayo274@gmail.com>
2025-05-06 17:39:11 +0300

    video-test-2

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
 
   serverExternalPackages: [],
 
+
+  api: {
+    bodyParser: {
+      sizeLimit: '100mb', 
+    },
+    responseLimit: '100mb',
+  },
+
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
