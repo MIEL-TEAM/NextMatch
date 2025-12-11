3d829ddff5fba2b9432e130fc10c79deb6d83240
almayomekonen <alamayo274@gmail.com>
2025-12-11 11:56:43 +0200

    new-version-12

---
+++ src/app/test-members/page.tsx
@@ -0,0 +1,17 @@
+import { getSession } from "@/lib/session";
+import MembersClient from "../members/MembersClient";
+
+export const metadata = {
+  title: "Test Members | Miel",
+  description: "Test page",
+};
+
+export default async function TestMembersPage() {
+  const session = await getSession();
+
+  if (!session) {
+    return <div>No session - please login</div>;
+  }
+
+  return <MembersClient serverSession={session} />;
+}
